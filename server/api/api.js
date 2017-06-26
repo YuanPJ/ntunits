0ac68173856780c8395d14e87a005cae27799677
Tiffany <tiffany85221@gmail.com>
2017-06-27 02:06:06 +0800

    api

--- server/api/api.js
+++ server/api/api.js
@@ -1,31 +1,7 @@
 import User from './dbModel';
-import '../Quizset';
 
-const data = [
-    {
-        question: "ㄅㄐ是什麼？",
-        options: [
-            "醜男",
-            "會長",
-            "阿海",
-            "天鈞的表哥",
-            "變態"
-        ]
-    },
-    {
-        question: "台大最大系？",
-        options: [
-            "醫學",
-            "管院各系",
-            "電機",
-            "文學院",
-            "工學院"
-        ]
-    }
-]
-
-
-  postUser(req, res){
+module.exports = {
+    postUser(req, res){
         const options = { upsert: true, new: true, setDefaultsOnInsert: true };
         User.findOneAndUpdate({userID: req.body.userID}, req.body, options)
     },
