f85e3806f421fdca6b64e800ff3ce396d6145efe
Bryan Wang <b03901041@ntu.edu.tw>
2017-06-29 01:04:52 +0800

    fix postUser

--- server/api/api.js
+++ server/api/api.js
@@ -1,12 +1,25 @@
 const User = require('./dbModel');
-console.log(User)
+// console.log(User)
 
 module.exports = {
   postUser(req, res) {
     // console.log('in the postUser')
-    const options = { upsert: true, new: true, setDefaultsOnInsert: true };
-    User.findOneAndUpdate({ userID: req.body.userID }, req.body, options);
+    // const options = { upsert: true, new: true, setDefaultsOnInsert: true };
+    // User.findOneAndUpdate({ userID: req.body.userID }, req.body, options);
+    User.count({userID: req.body.userID},(err,count) => {
+      if (count === 0){
+        console.log('create new user')
+        User.create({
+          userID: req.body.userID,
+          userName: req.body.userName,
+          userPicURI: req.body.userPicURI,
+          friendList: req.body.friendList
+        })
+      }
+    })
+
     console.log('postUser in server', req.body)
+    User.find({}, (err, user)=> console.log(user))
   },
   getUser(req, res) {
     const id = req.params.id;
--- src/containers/App.js
+++ src/containers/App.js
@@ -23,18 +23,18 @@ const setUserState = (login, id, name, pictureUrl, friends) => ({
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = this.props.state;
-    // this.state = {
-    //   login: false,
-    //   id: 0,
-    //   name: '',
-    //   pictureUrl: '',
-    //   friends: []
-    // };
+    // this.state = this.props.state;
+    this.state = {
+      login: false,
+      id: '',
+      name: '',
+      pictureUrl: '',
+      friends: []
+    };
     // this.setState(
       // this.props,
     // );
-    console.log('construct', this.props);
+    // console.log('construct', this.props);
     this.getFriends = this.getFriends.bind(this);
     this.didLogin = this.didLogin.bind(this);
     this.didNotLogin = this.didNotLogin.bind(this);
@@ -92,22 +92,37 @@ class App extends Component {
       });
     });
 
-    fetch('/api/user', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        userID: this.state.id,
-        userName: this.state.name,
-        userPicURI: this.state.pictureUrl,
-        friendList: this.state.friends
-      }),
-    })
+    // fetch('/api/user', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     userID: this.state.id,
+    //     userName: this.state.name,
+    //     userPicURI: this.state.pictureUrl,
+    //     friendList: this.state.friends
+    //   }),
+    // })
     setTimeout(this.props.setUserState(this.state.login, this.state.id, this.state.name, this.state.pictureUrl, this.state.friends), 1000);
-    setTimeout(() => console.log('didLogin', this.props), 2000);
-    setTimeout(() => console.log(this.state.login, this.state.id, this.state.name, this.state.pictureUrl, this.state.friends), 2000);
+    // setTimeout(() => console.log('didLogin', this.props), 2000);
+    setTimeout(() => {
+      console.log(this.state.login, this.state.id, this.state.name, this.state.pictureUrl, this.state.friends);
+      fetch('/api/user', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          userID: this.state.id,
+          userName: this.state.name,
+          userPicURI: this.state.pictureUrl,
+          friendList: this.state.friends
+        }),
+      })
+    }, 2000);
   }
   didNotLogin() {
     this.props.setState({
@@ -164,7 +179,22 @@ class App extends Component {
   //     friends={this.state.friends}/>
   // }
   render() {
-    console.log(this.state)
+    // console.log(this.state)
+    // if (this.state.id !== '' && this.state.name !== '' && this.state.pictureUrl !== ''){
+    //     fetch('/api/user', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Accept': 'application/json',
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //       userID: this.state.id,
+    //       userName: this.state.name,
+    //       userPicURI: this.state.pictureUrl,
+    //       friendList: this.state.friends
+    //     }),
+    //   })
+    // }
     const renderQuizPage = () => {
       return <QuizPage       
         login={this.state.login}
