978e7f87d474071439d3c9f391de4179e279af04
Tiffany <tiffany85221@gmail.com>
2017-06-28 14:25:50 +0800

    render question and options done

--- src/containers/QuizPage.js
+++ src/containers/QuizPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
+import Checkbox from 'material-ui/Checkbox';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './QuizPage.css';
 import QuizCell from '../components/QuizCell';
@@ -11,43 +12,44 @@ export default class QuizPage extends Component {
     super(props);
     this.state = {
       openDialog: false,
+      openChart: false,
       number: 0,
-<<<<<<< HEAD
-      data: {},
-=======
-      data: [{question:"", options:[""]}],
-      user: {userID: "", userName: "", userPicURI: "", friendList: [], answer: [0,0,0,0,0,0,0,0,0,0,0,0,0]}
->>>>>>> 0bab2beb9a4559aa7d3fc1bda170e9ccb7c7292c
+      data: [
+        { question: '', options: [''] },
+        { question: '', options: [''] },
+        { question: '', options: [''] },
+        { question: '', options: [''] },
+        { question: '', options: [''] },
+        { question: '', options: [''] },
+        { question: '', options: [''] },
+        { question: '', options: [''] },
+        { question: '', options: [''] },
+        { question: '', options: [''] },
+        { question: '', options: [''] },
+        { question: '', options: [''] },
+      ],
+      user: {
+        userID: '',
+        userName: '',
+        userPicURI: '',
+        friendList: [],
+        answer: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
     };
     this.handleClose = this.handleClose.bind(this);
   }
 
-<<<<<<< HEAD
-  componentWillMount() {
+  handleOpen(i) {
     fetch('/api/quiz')
       .then(res => res.json())
-      .then((data) => { this.setState({ data }); console.log(this.state.data)})
-      .catch((err) => { console.log(err); });
-  }
+      .then((quiz) => { this.setState({ data: quiz }); })
+      .catch((err) => { console.log('fetch get quiz error', err); });
+    this.setState({ openDialog: true, number: i });
 
-  handleOpen(i) {
-    this.setState({ openDialog: true, number: i + 1 });
-=======
-  handleOpen(i) {
-    // console.log(this.props.id)
-    fetch('/api/quiz')
-      .then(res => res.json())
-      .then((quiz) => { this.setState({ data: quiz}); })
-      .catch((err) => { console.log('fetch get quiz error',err); });
-    this.setState({ openDialog: true, number: i+1 });
-    
     fetch(`/api/user/${this.props.id}`)
       .then(res => res.json())
-      .then(user => this.setState({user}))
-      .catch(err => {console.log('fetch get user error',err)})
-
-    console.log('handleOpen state',this.state);
->>>>>>> 0bab2beb9a4559aa7d3fc1bda170e9ccb7c7292c
+      .then(user => this.setState({ user }))
+      .catch((err) => { console.log('fetch get user error', err); });
+    console.log('handleOpen state', this.state);
   }
 
   handleClose() {
@@ -55,10 +57,11 @@ export default class QuizPage extends Component {
   }
 
   render() {
-    console.log('quizpage state',this.state.data);
-    console.log('quizpage props',this.props)
-    const n = 13;
+    console.log('quizpage state', this.state.data);
+    console.log('quizpage props', this.props);
+    const n = 12;
     const list = Array.from(Array(n).keys());
+    const list_options = Array.from(Array(5).keys());
     const actions = [
       <FlatButton
         label="Cancel"
@@ -75,17 +78,28 @@ export default class QuizPage extends Component {
     return (
       <div className="container-fluid">
         <Dialog
-          title={this.state.data[0].question}
+          title={this.state.data[this.state.number].question}
           actions={actions}
           modal={false}
           open={this.state.openDialog}
           onRequestClose={this.handleClose}
           autoScrollBodyContent={true}
         >
-          {/*{this.state.data[this.state.number].question}*/}
-          {this.state.number}
+          {list_options.map(i =>
+            <Checkbox label={this.state.data[this.state.number].options[i]} key={`quizoptions-${i}`} />
+          )}
+        </Dialog>
+        <Dialog
+          title={this.state.data[this.state.number].question}
+          actions={actions}
+          modal={false}
+          open={this.state.openChart}
+          onRequestClose={this.handleClose}
+          autoScrollBodyContent={true}
+        >
           <BarChart />
         </Dialog>
+          
         <div className="row">
           {list.map(i =>
             (<div className="col-xs-6 col-sm-4 col-md-3 nopadding" key={`quizcell-${i}`}>
